0c511097d1483efaec0753eeb1b94ed033f167a4
ChandraBudiWijaya <chandrabw.cjcc@gmail.com>
2025-07-29 11:23:13 +0700

    feat: update Firebase initialization logic and remove deprecated files

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,14 @@
 const admin = require('firebase-admin');
 
 try {
-  const serviceAccount = require(`../${process.env.SERVICE_ACCOUNT_KEY_PATH}`);
+  let serviceAccount;
+  if (process.env.SERVICE_ACCOUNT_KEY_JSON) {
+    serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY_JSON);
+  } else if (process.env.SERVICE_ACCOUNT_KEY_PATH) {
+    serviceAccount = require(`../${process.env.SERVICE_ACCOUNT_KEY_PATH}`);
+  } else {
+    serviceAccount = require('../serviceaccountkey.json');
+  }
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
